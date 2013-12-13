75fb82c4d7c68e649e67a8d457f1e2e704d8272f
MIGUEL ANGEL <MIGUEL ANGEL@MIGUELANGEL-PC>
2013-12-13 17:09:11 +0100

    Finish

---
+++ src/questiansandanswersjava/Questiansandanswersjava.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questiansandanswersjava;
+
+/**
+ *
+ * @author MIGUEL ANGEL
+ */
+public class Questiansandanswersjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
